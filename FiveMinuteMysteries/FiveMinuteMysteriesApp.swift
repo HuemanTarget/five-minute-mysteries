fddd22166872d2842f8e012af088a9af02b8ada0
HuemanTarget <joshbasche@live.com>
2021-06-07 09:30:21 -0500

    first commit

---
+++ FiveMinuteMysteries/FiveMinuteMysteriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiveMinuteMysteriesApp.swift
+//  FiveMinuteMysteries
+//
+//  Created by Joshua Basche on 6/7/21.
+//
+
+import SwiftUI
+
+@main
+struct FiveMinuteMysteriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FiveMinuteMysteries/Model/TestModel.swift
@@ -0,0 +1,15 @@
+//
+//  TestModel.swift
+//  FiveMinuteMysteries
+//
+//  Created by Joshua Basche on 6/7/21.
+//
+
+import Foundation
+
+struct Product: Codable, Identifiable {
+  let id: String
+  let body: String
+  let choice: [String]
+  let nav: [String]
+}
