eefbc7287955886385b3ce4bba4a95acf11efac8
HuemanTarget <joshbasche@live.com>
2021-06-07 09:40:05 -0500

    create Constant file

--- FiveMinuteMysteries/Model/TestModel.swift
+++ FiveMinuteMysteries/Model/TestModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Product: Codable, Identifiable {
+struct Test: Codable, Identifiable {
   let id: String
   let body: String
   let choice: [String]
---
+++ FiveMinuteMysteries/Utility/Constant.swift
@@ -0,0 +1,10 @@
+//
+//  Constant.swift
+//  FiveMinuteMysteries
+//
+//  Created by Joshua Basche on 6/7/21.
+//
+
+import Foundation
+
+let tests: [Test] = Bundle.main.decode("fmmtest.json")
